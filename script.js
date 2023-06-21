72d39bb2ccceb978fc0cd5efe8f1dadf0054725b
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-06-21 21:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
+	let original = str;
+	let revStr = str.reverse();
+	if(original===revStr)
+		true;
+	else
+		false;
 
 }
 module.exports = palindrome
