310d1d30268549f60fd095b05253dd16e4837ecb
Mohd Shadab <89182232+shadabibrahim@users.noreply.github.com>
2023-06-22 15:32:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 // complete the given function
 
-function palindrome(str){
-	let original = str;
-	let arrStr = str.split();
-	let rev = arrStr.revrse();
-	let ans = rev.join('');
-	if(original===ans)
-		true;
-	else
-		false;
-
+function palindrome(str1){
+let startIndex= 0;
+let str = str1.toLowerCase();
+let endIndex = str.length-1;
+	while(startIndex < endIndex){
+		if(str[startIndex] != str[endIndex]){
+			return false;
+		}
+		startIndex++;
+		endIndex--;
+	}
+	return true;
 }
 module.exports = palindrome
